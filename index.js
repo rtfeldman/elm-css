c9a865dab716fac741062a64a1b4d083cc5c6e31
Richard Feldman <richard.t.feldman@gmail.com>
2015-12-22 09:47:48 -0800

    Externalize index.js API

--- index.js
+++ index.js
@@ -6,49 +6,43 @@ var compile = require("node-elm-compiler").compile;
 var emitterFilename = "emitter.js";
 
 
-// TODO: externalize all this and make it configurable
-var srcFile = "examples/src/HomepageStylesheet.elm";
-var elmModuleName = "HomepageStylesheet";
-var destCssFile = path.join(__dirname, "output.css")
-process.chdir("examples")
-// END TODO
-
-
-tmp.dir({unsafeCleanup: true}, function (err, tmpDirPath, cleanup) {
-  if (err) {
-    throw err;
-  }
-
-  var emitterDest = path.join(tmpDirPath, emitterFilename);
-
-  compileEmitter({
-    output: emitterDest
-  }, function() {
-    // Make the compiled emitter.js Node-friendly.
-    fs.appendFileSync(emitterDest, "\nmodule.exports = Elm;");
-
-    var Elm = require(emitterDest);
-    var result = Elm.worker(Elm[elmModuleName]).ports.cssOutput;
-
-    if(result.success) {
-      fs.writeFileSync(destCssFile, result.content + "\n");
-    } else {
-      console.error("Compilation error: " + result.content, 1);
-      process.exit(1);
+module.exports = function(srcFile, elmModuleName, destCssFile) {
+  tmp.dir({unsafeCleanup: true}, function (err, tmpDirPath, cleanup) {
+    if (err) {
+      throw err;
     }
-  }, function(exitCode) {
-    console.error("Errored with exit code", exitCode);
-    process.exit(exitCode);
-  });
-});
 
-function compileEmitter(options, onSuccess, onError) {
-  compile(path.join(__dirname, srcFile), options)
-    .on("close", function(exitCode) {
-      if (exitCode === 0) {
-        onSuccess()
+    var emitterDest = path.join(tmpDirPath, emitterFilename);
+
+    compileEmitter({
+      output: emitterDest
+    }, function() {
+      // Make the compiled emitter.js Node-friendly.
+      fs.appendFileSync(emitterDest, "\nmodule.exports = Elm;");
+
+      var Elm = require(emitterDest);
+      var result = Elm.worker(Elm[elmModuleName]).ports.cssOutput;
+
+      if(result.success) {
+        fs.writeFileSync(destCssFile, result.content + "\n");
       } else {
-        onError(exitCode);
+        console.error("Compilation error: " + result.content, 1);
+        process.exit(1);
       }
+    }, function(exitCode) {
+      console.error("Errored with exit code", exitCode);
+      process.exit(exitCode);
     });
+  });
+
+  function compileEmitter(options, onSuccess, onError) {
+    compile(path.join(__dirname, srcFile), options)
+      .on("close", function(exitCode) {
+        if (exitCode === 0) {
+          onSuccess()
+        } else {
+          onError(exitCode);
+        }
+      });
+  }
 }
