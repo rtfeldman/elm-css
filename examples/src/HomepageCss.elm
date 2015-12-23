a82b7721baf8d895e3b4f5c2c1c8b72d4814cade
Richard Feldman <richard.t.feldman@gmail.com>
2015-12-23 00:27:51 -0800

    Fix formatting in HomepageCss.elm

--- examples/src/HomepageCss.elm
+++ examples/src/HomepageCss.elm
@@ -7,25 +7,29 @@ import SharedStyles exposing (..)
 css =
     stylesheet { name = "homepage" }
         $ header
-        ~ backgroundColor (rgb 90 90 90)
-        ~ boxSizing borderBox
-        ~ padding -80 px
+            ~ backgroundColor (rgb 90 90 90)
+            ~ boxSizing borderBox
+            ~ padding -80 px
+
         $ nav
-        ~ display inlineBlock
-        ~ paddingBottom 12 px
+            ~ display inlineBlock
+            ~ paddingBottom 12 px
+
         . NavLink
-        ~ margin 12 px
-        ~ color (rgb 255 255 255)
+            ~ margin 12 px
+            ~ color (rgb 255 255 255)
+
         # ReactiveLogo
-        ~ display inlineBlock
-        ~ marginLeft 150 px
-        ~ marginRight 80 px
-        ~ verticalAlign middle
+            ~ display inlineBlock
+            ~ marginLeft 150 px
+            ~ marginRight 80 px
+            ~ verticalAlign middle
+
         # BuyTickets
-        ~ padding 16 px
-        ~ paddingLeft 24 px
-        ~ paddingRight 24 px
-        ~ marginLeft 50 px
-        ~ color (rgb 255 255 255)
-        ~ backgroundColor (rgb 27 217 130)
-        ~ verticalAlign middle
+            ~ padding 16 px
+            ~ paddingLeft 24 px
+            ~ paddingRight 24 px
+            ~ marginLeft 50 px
+            ~ color (rgb 255 255 255)
+            ~ backgroundColor (rgb 27 217 130)
+            ~ verticalAlign middle
