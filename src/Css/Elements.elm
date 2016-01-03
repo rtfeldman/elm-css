bbbd46ddf38bc37a83ee3ac3254555b90edaf23e
Luke Westby <luke.westby@raise.com>
2016-01-03 14:13:32 -0600

    html elements

--- src/Css/Elements.elm
+++ src/Css/Elements.elm
@@ -1,15 +1,55 @@
-module Css.Elements (Tag, tagToString, a, html, body, header, nav, div, span, img, nowrap, button, h1, h2, h3, h4, p, ol, input) where
+module Css.Elements
+    ( Tag, tagToString
+    , html, body
+    , article, header, footer, h1, h2, h3, h4, nav, section
+    , div, hr, li, main', ol, p, ul, pre
+    , a, code, em, small, span, strong
+    , img, audio, video, canvas
+    , caption, col, colgroup, table, tbody, td, tfoot, th, thead, tr
+    , button, fieldset, form, input, label, legend, optgroup, option, progress, select
+    ) where
 
 {-| Selectors for HTML elements.
 
 @docs Tag, tagToString
 
-# Elements
-@docs a, html, body, header, nav, div, span, img, nowrap, button, h1, h2, h3, h4, p, ol, input
+# Basic elements
+@docs html, body
+
+# Content sectioning
+@docs article, header, footer, h1, h2, h3, h4, nav, section
+
+# Text content
+@docs div, hr, li, main', ol, p, ul, pre
+
+# Inline text semantics
+@docs a, code, em, small, span, strong
+
+# Image and multimedia
+@docs img, audio, video, canvas
+
+# Table content
+@docs caption, col, colgroup, table, tbody, td, tfoot, th, thead, tr
+
+# Forms
+@docs button, fieldset, form, input, label, legend, optgroup, option, progress, select
 -}
 
-{- Tags -}
 
+{-| Selector for an HTML tag.
+-}
+type Tag
+    = Tag String
+
+
+{-| Converts an HTML tag selector to its underlying String representation.
+-}
+tagToString : Tag -> String
+tagToString (Tag str) =
+    str
+
+
+{- BASIC ELEMENTS -}
 
 {-| Selector for a html element.
 -}
@@ -25,6 +65,16 @@ body =
     Tag "body"
 
 
+{- CONTENT SECTIONING -}
+
+
+{-| Selector for an article element.
+-}
+article : Tag
+article =
+    Tag "article"
+
+
 {-| Selector for a header element.
 -}
 header : Tag
@@ -32,53 +82,11 @@ header =
     Tag "header"
 
 
-{-| Selector for an `<a>` element.
+{-| Selector for a footer element.
 -}
-a : Tag
-a =
-    Tag "a"
-
-
-{-| Selector for a nav element.
--}
-nav : Tag
-nav =
-    Tag "nav"
-
-
-{-| Selector for a div element.
--}
-div : Tag
-div =
-    Tag "div"
-
-
-{-| Selector for a span element.
--}
-span : Tag
-span =
-    Tag "span"
-
-
-{-| Selector for a img element.
--}
-img : Tag
-img =
-    Tag "img"
-
-
-{-| Selector for a nowrap element.
--}
-nowrap : Tag
-nowrap =
-    Tag "nowrap"
-
-
-{-| Selector for a button element.
--}
-button : Tag
-button =
-    Tag "button"
+footer : Tag
+footer =
+    Tag "footer"
 
 
 {-| Selector for an h1 element.
@@ -109,11 +117,49 @@ h4 =
     Tag "h4"
 
 
-{-| Selector for a p element.
+{-| Selector for a nav element.
 -}
-p : Tag
-p =
-    Tag "p"
+nav : Tag
+nav =
+    Tag "nav"
+
+
+{-| Selector for a section element.
+-}
+section : Tag
+section =
+    Tag "section"
+
+
+{- TEXT CONTENT -}
+
+
+{-| Selector for a div element.
+-}
+div : Tag
+div =
+    Tag "div"
+
+
+{-| Selector for an hr element.
+-}
+hr : Tag
+hr =
+    Tag "hr"
+
+
+{-| Selector for an li element.
+-}
+li : Tag
+li =
+    Tag "li"
+
+
+{-| Selector for a main element.
+-}
+main' : Tag
+main' =
+    Tag "main"
 
 
 {-| Selector for an ol element.
@@ -123,6 +169,201 @@ ol =
     Tag "ol"
 
 
+{-| Selector for a p element.
+-}
+p : Tag
+p =
+    Tag "p"
+
+
+{-| Selector for a ul element.
+-}
+ul : Tag
+ul =
+    Tag "ul"
+
+
+{-| Selector for a pre element.
+-}
+pre : Tag
+pre =
+    Tag "pre"
+
+{- INLINE TEXT SEMANTICS -}
+
+
+
+{-| Selector for an `<a>` element.
+-}
+a : Tag
+a =
+    Tag "a"
+
+
+{-| Selector for a code element.
+-}
+code : Tag
+code =
+    Tag "code"
+
+
+{-| Selector for an em element.
+-}
+em : Tag
+em =
+    Tag "em"
+
+
+{-| Selector for a small element.
+-}
+small : Tag
+small =
+    Tag "small"
+
+
+{-| Selector for a span element.
+-}
+span : Tag
+span =
+    Tag "span"
+
+
+{-| Selector for a strong element.
+-}
+strong : Tag
+strong =
+    Tag "strong"
+
+
+{-| IMAGE AND MULTIMEDIA -}
+
+
+{-| Selector for a img element.
+-}
+img : Tag
+img =
+    Tag "img"
+
+
+{-| Selector for an audio element.
+-}
+audio : Tag
+audio =
+    Tag "audio"
+
+
+{-| Selector for a video element.
+-}
+video : Tag
+video =
+    Tag "video"
+
+
+{-| Selector for a canvas element.
+-}
+canvas : Tag
+canvas =
+    Tag "canvas"
+
+
+
+{- TABLE CONTENT -}
+
+
+{-| Selector for a caption element.
+-}
+caption : Tag
+caption =
+    Tag "caption"
+
+
+{-| Selector for a col element.
+-}
+col : Tag
+col =
+    Tag "col"
+
+
+{-| Selector for a colgroup element.
+-}
+colgroup : Tag
+colgroup =
+    Tag "colgroup"
+
+
+{-| Selector for a table element.
+-}
+table : Tag
+table =
+    Tag "table"
+
+
+{-| Selector for a tbody element.
+-}
+tbody : Tag
+tbody =
+    Tag "tbody"
+
+
+{-| Selector for a td element.
+-}
+td : Tag
+td =
+    Tag "td"
+
+
+{-| Selector for a tfoot element.
+-}
+tfoot : Tag
+tfoot =
+    Tag "tfoot"
+
+
+{-| Selector for a th element.
+-}
+th : Tag
+th =
+    Tag "th"
+
+
+{-| Selector for a thead element.
+-}
+thead : Tag
+thead =
+    Tag "thead"
+
+
+{-| Selector for a thead element.
+-}
+tr : Tag
+tr =
+    Tag "tr"
+
+
+{- FORMS -}
+
+
+{-| Selector for a button element.
+-}
+button : Tag
+button =
+    Tag "button"
+
+
+{-| Selector for a fieldset element.
+-}
+fieldset : Tag
+fieldset =
+    Tag "fieldset"
+
+
+{-| Selector for a form element.
+-}
+form : Tag
+form =
+    Tag "form"
+
+
 {-| Selector for an input element.
 -}
 input : Tag
@@ -130,14 +371,43 @@ input =
     Tag "input"
 
 
-{-| Selector for an HTML tag.
+{-| Selector for a label element.
 -}
-type Tag
-    = Tag String
+label : Tag
+label =
+    Tag "label"
 
 
-{-| Converts an HTML tag selector to its underlying String representation.
+{-| Selector for a legend element.
 -}
-tagToString : Tag -> String
-tagToString (Tag str) =
-    str
+legend : Tag
+legend =
+    Tag "legend"
+
+
+{-| Selector for an optgroup element.
+-}
+optgroup : Tag
+optgroup =
+    Tag "optgroup"
+
+
+{-| Selector for an option element.
+-}
+option : Tag
+option =
+    Tag "option"
+
+
+{-| Selector for a progress element.
+-}
+progress : Tag
+progress =
+    Tag "progress"
+
+
+{-| Selector for a select element.
+-}
+select : Tag
+select =
+    Tag "select"
