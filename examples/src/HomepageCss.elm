0bf2434a708fa31df59f7e5c440d72cc4e2834cc
Richard Feldman <richard.t.feldman@gmail.com>
2016-01-18 08:29:17 -0800

    Update HomepageCss to use new style.

--- examples/src/HomepageCss.elm
+++ examples/src/HomepageCss.elm
@@ -6,32 +6,35 @@ import SharedStyles exposing (..)
 
 
 css =
-    stylesheet { name = "homepage" }
-        $ header
-            ~ backgroundColor (rgb 90 90 90)
-            ~ boxSizing borderBox
-            ~ padding (px -80)
-
-        $ nav
-            ~ display inlineBlock
-            ~ paddingBottom (px 12)
-
-        . NavLink
-            ~ margin (px 12)
-            ~ color (rgb 255 255 255)
-
-        # ReactiveLogo
-            ~ display inlineBlock
-            ~ marginLeft (px 150)
-            ~ marginRight (px 80)
-            ~ verticalAlign middle
-
-        # BuyTickets
-            ~ padding (px 16)
-            ~ paddingLeft (px 24)
-            ~ paddingRight (px 24)
-            ~ marginLeft (px 50)
-            ~ marginRight auto
-            ~ color (rgb 255 255 255)
-            ~ backgroundColor (rgb 27 217 130)
-            ~ verticalAlign middle
+  stylesheet
+    { name = "homepage" }
+    [ header
+        [ backgroundColor (rgb 90 90 90)
+        , boxSizing borderBox
+        , padding (px -80)
+        ]
+    , nav
+        [ display inlineBlock
+        , paddingBottom (px 12)
+        ]
+    , ((.) NavLink)
+        [ margin (px 12)
+        , color (rgb 255 255 255)
+        ]
+    , ((#) ReactiveLogo)
+        [ display inlineBlock
+        , marginLeft (px 150)
+        , marginRight (px 80)
+        , verticalAlign middle
+        ]
+    , ((#) BuyTickets)
+        [ padding (px 16)
+        , paddingLeft (px 24)
+        , paddingRight (px 24)
+        , marginLeft (px 50)
+        , marginRight auto
+        , color (rgb 255 255 255)
+        , backgroundColor (rgb 27 217 130)
+        , verticalAlign middle
+        ]
+    ]
