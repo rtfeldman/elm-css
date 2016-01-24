44bd1b57f6bced73aaccbe9e6faafb140a405e94
Richard Feldman <richard.t.feldman@gmail.com>
2016-01-24 08:29:36 -0600

    Update Helpers to distinguish between namespaced and not, and to make class take a List

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -11,6 +11,13 @@ import Html.Attributes as Attr
 import String
 
 
+type alias Helpers class id =
+  { class : class -> Attribute
+  , classList : List ( class, Bool ) -> Attribute
+  , id : id -> Attribute
+  }
+
+
 type alias Namespace name class id =
   { class : class -> Attribute
   , classList : List ( class, Bool ) -> Attribute
@@ -21,13 +28,16 @@ type alias Namespace name class id =
 
 {-| Takes a namespace and returns helper functions for `id`, `class`, and
 `classList` which work just like their equivalents in `elm-html` except that
-they accept union types and automatically incorporate the given namespace.
+they accept union types and automatically incorporate the given namespace. Also
+note that `class` accepts a `List` instead of a single element; this is so you
+can specify multiple classes without having to call `classList` passing tuples
+that all end in `True`.
 
     -- Put this before your view code to specify a namespace.
     { id, class, classList } = namespace "homepage"
 
     view =
-        h1 [ id Hero, class Fancy ] [ text "Hello, World!" ]
+        h1 [ id Hero, class [ Fancy ] ] [ text "Hello, World!" ]
 
     type HomepageIds = Hero | SomethingElse | AnotherId
     type HomepageClasses = Fancy | AnotherClass | SomeOtherClass
@@ -38,17 +48,43 @@ The above would generate this DOM element:
 -}
 namespace : name -> Namespace name class id
 namespace name =
-  { class = identifierToString name >> Attr.class
-  , classList = classList name
+  { class = namespacedClass name
+  , classList = namespacedClassList name
   , id = toCssIdentifier >> Attr.id
   , name = name
   }
 
 
-classList : name -> List ( class, Bool ) -> Attribute
-classList name list =
+helpers : Helpers
+helpers =
+  { class = class
+  , classList = classList
+  , id = toCssIdentifier >> Attr.id
+  }
+
+
+namespacedClassList : name -> List ( class, Bool ) -> Attribute
+namespacedClassList name list =
   list
     |> List.filter snd
-    |> List.map (fst >> identifierToString name)
-    |> String.join " "
-    |> Attr.class
+    |> List.map fst
+    |> namespacedClass name
+
+
+classList : List ( class, Bool ) -> Attribute
+classList list =
+  list
+    |> List.filter snd
+    |> List.map fst
+    |> class
+
+
+namespacedClass : name -> List class -> Attribute
+namespacedClass name list =
+  list
+    |> List.map (identifierToString name)
+
+
+class : List class -> Attribute
+class =
+  String.join " " >> Attr.class
