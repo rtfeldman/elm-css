4995eb078b3dbde45baccf0850cb56bbfd213f1b
Richard Feldman <richard.t.feldman@gmail.com>
2016-06-17 18:03:47 -0700

    Upgrade to new version of project-fuzzball-test

--- test/TestUtil.elm
+++ test/TestUtil.elm
@@ -39,7 +39,7 @@ validAlphaValue =
 
 invalidRgbValue : Fuzzer Int
 invalidRgbValue =
-    frequencyOrCrash
+    Fuzz.frequencyOrCrash
         [ ( 1, Fuzz.intRange -300 -1 )
         , ( 1, Fuzz.intRange 256 300 )
         ]
@@ -47,48 +47,7 @@ invalidRgbValue =
 
 invalidAlphaValue : Fuzzer Float
 invalidAlphaValue =
-    frequencyOrCrash
+    Fuzz.frequencyOrCrash
         [ ( 1, Fuzz.floatRange -300 -1.0e-3 )
         , ( 1, Fuzz.floatRange 1.0001 300 )
         ]
-
-
-frequency : List ( Float, Fuzzer a ) -> Result String (Fuzzer a)
-frequency list =
-    case List.head list of
-        Nothing ->
-            Err "You must provide at least one frequency pair."
-
-        Just ( _, { shrinker } ) ->
-            if List.any (\( weight, _ ) -> weight < 0) list then
-                Err "No frequency weights can be less than 0."
-            else if List.sum (List.map fst list) <= 0 then
-                Err "Frequency weights must sum to more than 0."
-            else
-                let
-                    generator =
-                        list
-                            |> List.map toGeneratorFrequency
-                            |> Random.frequency
-                in
-                    Ok (Fuzzer generator shrinker)
-
-
-toGeneratorFrequency : ( Float, Fuzzer a ) -> ( Float, Generator a )
-toGeneratorFrequency ( weight, { generator } ) =
-    ( weight, generator )
-
-
-frequencyOrCrash : List ( Float, Fuzzer a ) -> Fuzzer a
-frequencyOrCrash =
-    frequency >> okOrCrash
-
-
-okOrCrash : Result String a -> a
-okOrCrash result =
-    case result of
-        Ok a ->
-            a
-
-        Err str ->
-            Debug.crash str
