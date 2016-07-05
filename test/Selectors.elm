68122597a13fe89788a413f17f646fae13e1bd06
Richard Feldman <richard.t.feldman@gmail.com>
2016-07-05 08:28:05 -0700

    Add a bunch of element tests.

--- test/Selectors.elm
+++ test/Selectors.elm
@@ -29,6 +29,56 @@ elements =
     describe "selectors"
         [ testSelector "ol" ol
         , testSelector "ul" ul
+        , testSelector "html" html
+        , testSelector "body" body
+        , testSelector "article" article
+        , testSelector "header" header
+        , testSelector "footer" footer
+        , testSelector "h1" h1
+        , testSelector "h2" h2
+        , testSelector "h3" h3
+        , testSelector "h4" h4
+        , testSelector "h5" h5
+        , testSelector "h6" h6
+        , testSelector "nav" nav
+        , testSelector "section" section
+        , testSelector "div" div
+        , testSelector "hr" hr
+        , testSelector "li" li
+        , testSelector "main" main'
+        , testSelector "ol" ol
+        , testSelector "p" p
+        , testSelector "ul" ul
+        , testSelector "pre" pre
+        , testSelector "a" a
+        , testSelector "code" code
+        , testSelector "small" Css.Elements.small
+        , testSelector "span" span
+        , testSelector "strong" strong
+        , testSelector "img" img
+        , testSelector "audio" audio
+        , testSelector "video" video
+        , testSelector "canvas" canvas
+        , testSelector "caption" caption
+        , testSelector "col" col
+        , testSelector "colgroup" colgroup
+        , testSelector "table" table
+        , testSelector "tbody" tbody
+        , testSelector "td" td
+        , testSelector "tfoot" tfoot
+        , testSelector "th" th
+        , testSelector "thead" thead
+        , testSelector "tr" tr
+        , testSelector "button" button
+        , testSelector "fieldset" fieldset
+        , testSelector "form" form
+        , testSelector "input" input
+        , testSelector "label" label
+        , testSelector "legend" legend
+        , testSelector "optgroup" optgroup
+        , testSelector "option" option
+        , testSelector "progress" progress
+        , testSelector "select" select
         ]
 
 
