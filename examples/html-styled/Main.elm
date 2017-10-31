df42447f91d45576bfe86cef7c29c7fd8c43c8e9
Richard Feldman <richard.t.feldman@gmail.com>
2017-10-31 09:58:21 +0200

    Update example

--- examples/html-styled/Main.elm
+++ examples/html-styled/Main.elm
@@ -2,19 +2,17 @@ module Main exposing (main)
 
 import Css exposing (..)
 import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (css, src, withCss)
-import Html.Styled.Lazy exposing (lazy)
+import Html.Styled.Attributes exposing (css, href, src, withCss)
 
 
-headerFont : Style
 headerFont =
     batch
         [ fontFamilies [ "Between1-Regular", "sans-serif" ]
         , fontSize (px 16)
+        , lineHeight (num 1.2)
         ]
 
 
-pageHeader : List (Attribute msg) -> List (Html msg) -> Html msg
 pageHeader =
     withCss header
         [ headerFont
@@ -23,25 +21,22 @@ pageHeader =
         ]
 
 
-navigation : List (Attribute msg) -> List (Html msg) -> Html msg
 navigation =
     withCss nav
         [ display inlineBlock
-        , height (px 100)
+        , height (px 40)
         , paddingTop (px 40)
         , paddingBottom (px 12)
         ]
 
 
-navLink : List (Attribute msg) -> List (Html msg) -> Html msg
 navLink =
     withCss a
         [ margin (px 12)
-        , color (rgb 255 255 255)
+        , color (rgb 250 250 250)
         ]
 
 
-logo : List (Attribute msg) -> List (Html msg) -> Html msg
 logo =
     withCss img
         [ display inlineBlock
@@ -52,56 +47,42 @@ logo =
         ]
 
 
-buyTickets : List (Attribute msg) -> List (Html msg) -> Html msg
-buyTickets =
-    withCss button
-        [ headerFont
-        , backgroundColor transparent
-        , width (px 170)
-        , height (px 40)
-        , marginLeft (px 50)
-        , color (rgb 255 255 255)
-        , border3 (px 5) solid reactiveGreen
-        , verticalAlign middle
-        ]
+reactiveConfColor =
+    hex "55af6a"
 
 
-viewBuyTickets caption =
-    buyTickets [] [ text caption ]
-
-
-main =
-    view "BUY TICKETS"
-        |> toUnstyled
-
-
-backdrop =
-    linearGradient (stop (hex "1b0c39")) (stop (hex "175169")) [ stop (hex "56b36d") ]
-
-
-headerGradient =
-    linearGradient (stop2 (hex "060b24") (px 0)) (stop2 (hex "060b24") (px 200)) [ stop2 (rgba 0 0 0 0) (px 600) ]
-
-
-reactiveGreen =
-    -- hex "55af6a"
-    rgb 121 207 232
-
-
-view ticketsCaption =
-    div [ css [ backgroundImage headerGradient ] ]
-        [ pageHeader []
-            [ logo [ src "assets/reactive-conf.svg" ] []
-            , navigation [] navElems
-            , span [ css [ color reactiveGreen ] ] [ text "Oct 25–27, 2017" ]
-            , lazy viewBuyTickets ticketsCaption
-            ]
-        , div []
-            [ img [ src "assets/banner.png" ] [] ]
-        ]
-
-
-navElems : List (Html msg)
 navElems =
     [ "SPEAKERS", "WORKSHOPS", "SCHEDULE", "VENUE", "CONTACT", "ABOUT US" ]
         |> List.map (\name -> navLink [] [ text name ])
+
+
+view =
+    div []
+        [ pageHeader []
+            [ logo [ src "assets/reactive-conf.svg" ] []
+            , navigation [] navElems
+            , span
+                [ css [ color reactiveConfColor ] ]
+                [ text "Oct 25–27, 2017" ]
+            , a
+                [ href "/tickets"
+                , css
+                    [ display inlineBlock
+                    , color (rgb 250 250 250)
+                    , marginLeft (px 50)
+                    , border3 (px 5) solid reactiveConfColor
+                    , padding2 (px 12) (px 16)
+                    , textDecoration none
+                    , headerFont
+                    , hover [ backgroundColor reactiveConfColor ]
+                    ]
+                ]
+                [ text "BUY TICKETS" ]
+            ]
+        , div []
+            [ img [ src "assets/backdrop.jpg", css [ width (pct 100) ] ] [] ]
+        ]
+
+
+main =
+    view |> toUnstyled
