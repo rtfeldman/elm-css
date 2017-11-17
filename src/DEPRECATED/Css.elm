af214b8f314f5d5852a750b1f20558eadb80c507
Richard Feldman <richard.t.feldman@gmail.com>
2017-11-16 23:07:20 -0500

    Re-add asPairs, but deprecate it.

---
+++ src/DEPRECATED/Css.elm
@@ -0,0 +1,31 @@
+module DEPRECATED.Css exposing (asPairs)
+
+{-| Deprecated functions.
+
+Okay, deprecated _function_.
+
+Just the one, really.
+
+@docs asPairs
+
+-}
+
+import Css exposing (Style)
+import Css.Preprocess
+
+
+{-| **DEPRECATED**. Like compiling to .css files, and support for `asPairs` planned to be removed in a
+future release (likely sometime in 2018), once [`Html.Styled`](Html-Styled) has
+matured sufficiently. The design goal is for elm-css to offer One Way To Do It,
+and that one way is planned to be `Html.Styled`!
+
+Take a list of styles and return a list of key-value pairs that
+can then be passed to a `style` attribute.
+styled = asPairs >> Html.Attributes.style
+button
+[ styled [ position absolute, left (px 5) ] ][ text "Whee!" ]
+
+-}
+asPairs : List Style -> List ( String, String )
+asPairs =
+    Css.Preprocess.toPropertyPairs
