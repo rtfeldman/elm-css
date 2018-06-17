28b428859324783afcad457bd9e202f96e557ca2
Eduard Kovalov <ed@edkovalov.com>
2018-06-17 16:43:05 +0300

    Improve Lazy API, add more lazyN functions

--- src/Html/Styled/Lazy.elm
+++ src/Html/Styled/Lazy.elm
@@ -1,34 +1,22 @@
-module Html.Styled.Lazy exposing (lazy, lazy2, lazy3)
+module Html.Styled.Lazy
+    exposing
+        ( lazy
+        , lazy2
+        , lazy3
+        , lazy4
+        , lazy5
+        , lazy6
+        , lazy7
+        )
 
-{-| A not-quite-drop-in replacement for the `Html.Lazy` module from the
-`elm-lang/html` package.
+{-| Drop-in replacement for the `Html.Lazy` module from the `elm/html` package.
+Well, almost — the only difference is that there is no `lazy8` function.
 
-The only difference between this and the `lazy` from `elm-lang/html` is that the
-caller must call `toUnstyled` inside the function being passed to `lazy`.
-
-For example:
-
-        import Html.Styled as Html exposing (Html)
-        import Html.Styled.Lazy exposing (lazy)
-        import VirtualDom
-
-
-        view : Model -> Html Msg
-        view model =
-            lazy viewCount model.count
-
-
-        viewCount : Int -> VirtualDom.Node msg
-        viewCount count =
-            span [ css [ textDecoration underline ] ] [ text (toString count) ]
-                |> toUnstyled -- toUnstyled must happen inside viewCount!
-
-@docs lazy, lazy2, lazy3
+@docs lazy, lazy2, lazy3, lazy4, lazy5, lazy6, lazy7
 
 -}
 
 import Html.Styled as Html exposing (Html)
-import VirtualDom
 import VirtualDom.Styled
 
 
@@ -39,20 +27,48 @@ can check to see if `model` is referentially equal to the previous value used,
 and if so, we just stop. No need to build up the tree structure and diff it,
 we know if the input to `view` is the same, the output must be the same!
 -}
-lazy : (a -> VirtualDom.Node msg) -> a -> Html msg
+lazy : (a -> Html msg) -> a -> Html msg
 lazy =
     VirtualDom.Styled.lazy
 
 
 {-| Same as `lazy` but checks on two arguments.
 -}
-lazy2 : (a -> b -> VirtualDom.Node msg) -> a -> b -> Html msg
+lazy2 : (a -> b -> Html msg) -> a -> b -> Html msg
 lazy2 =
     VirtualDom.Styled.lazy2
 
 
 {-| Same as `lazy` but checks on three arguments.
 -}
-lazy3 : (a -> b -> c -> VirtualDom.Node msg) -> a -> b -> c -> Html msg
+lazy3 : (a -> b -> c -> Html msg) -> a -> b -> c -> Html msg
 lazy3 =
     VirtualDom.Styled.lazy3
+
+
+{-| Same as `lazy` but checks on four arguments.
+-}
+lazy4 : (a -> b -> c -> d -> Html msg) -> a -> b -> c -> d -> Html msg
+lazy4 =
+    VirtualDom.Styled.lazy4
+
+
+{-| Same as `lazy` but checks on five arguments.
+-}
+lazy5 : (a -> b -> c -> d -> e -> Html msg) -> a -> b -> c -> d -> e -> Html msg
+lazy5 =
+    VirtualDom.Styled.lazy5
+
+
+{-| Same as `lazy` but checks on six arguments.
+-}
+lazy6 : (a -> b -> c -> d -> e -> f -> Html msg) -> a -> b -> c -> d -> e -> f -> Html msg
+lazy6 =
+    VirtualDom.Styled.lazy6
+
+
+{-| Same as `lazy` but checks on seven arguments.
+-}
+lazy7 : (a -> b -> c -> d -> e -> f -> g -> Html msg) -> a -> b -> c -> d -> e -> f -> g -> Html msg
+lazy7 =
+    VirtualDom.Styled.lazy7
