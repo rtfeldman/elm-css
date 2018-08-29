aff1cce979b89b77b1ffbdbb51ee9585ddf25980
Richard Feldman <richard.t.feldman@gmail.com>
2018-08-29 15:08:57 -0400

    :art: elm-format

--- src/Css.elm
+++ src/Css.elm
@@ -2294,13 +2294,13 @@ type PxUnits
     = PxUnits
 
 
-{-| [\`\`](<https://developer.mozilla.org/en-US/docs/Web/CSS/length#mm>) units.
+{-| [\`\`](https://developer.mozilla.org/en-US/docs/Web/CSS/length#mm) units.
 -}
 type alias Mm =
     ExplicitLength MMUnits
 
 
-{-| [\`\`](<https://developer.mozilla.org/en-US/docs/Web/CSS/length#mm>) units.
+{-| [\`\`](https://developer.mozilla.org/en-US/docs/Web/CSS/length#mm) units.
 -}
 mm : Float -> Mm
 mm =
