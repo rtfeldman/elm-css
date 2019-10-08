e2096e8f995cdfd2144f21e4b705aa3585ababfb
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2019-10-07 20:22:33 -0500

    Add listStylePosition

--- src/Css.elm
+++ src/Css.elm
@@ -52,7 +52,7 @@ module Css exposing
     , notAllowed, allScroll, colResize, rowResize, nResize, eResize, sResize
     , wResize, neResize, nwResize, seResize, swResize, ewResize, nsResize
     , neswResize, nwseResize, zoomIn, zoomOut, grab, grabbing
-    , listStyle, listStyle2, listStyle3
+    , listStyle, listStyle2, listStyle3, listStylePosition
     , arabicIndic, armenian, bengali, cjkEarthlyBranch, cjkHeavenlyStem, devanagari, georgian, gujarati, gurmukhi, kannada, khmer, lao, malayalam, myanmar, oriya, telugu, thai
     , auto, none
     , hidden, visible
@@ -71,7 +71,7 @@ module Css exposing
     , capitalize, uppercase, lowercase, fullWidth
     , textDecoration, textDecoration2, textDecoration3, textDecorationLine, textDecorationLine2, textDecorationLine3, textDecorationStyle, textDecorationColor
     , wavy, underline, overline, lineThrough
-    , borderCollapse
+    , borderCollapse 
     , collapse, separate
     , borderSpacing, borderSpacing2
     , captionSide
@@ -5812,6 +5812,45 @@ listStyle3 :
 listStyle3 (Value val1) (Value val2) (Value val3) =
     AppendProperty ("list-style:" ++ val1 ++ " " ++ val2 ++ " " ++ val3)
 
+
+{-| The [`list-style-position`](https://developer.mozilla.org/en-US/docs/Web/CSS/list-style-position) property
+
+    listStylePosition inside
+    
+    listStylePosition outside
+
+-}
+listStylePosition :
+    Value
+        { inside : Supported
+        , outside : Supported
+        , inherit : Supported
+        , initial : Supported
+        , unset : Supported
+        }
+    -> Style
+listStylePosition (Value pos) =
+    AppendProperty ("list-style-position:" ++ pos)
+
+
+{-| The `inside` value for [`list-style-position`](#listStylePosition)
+
+    listStylePosition inside
+
+-}
+inside : Value { provides | inside : Supported } 
+inside =
+    Value "inside"
+
+
+{-| The `inside` value for [`list-style-position`](#listStylePosition)
+
+    listStylePosition outside
+
+-}
+outside : Value { provides | outside : Supported }
+outside =
+    Value "outside"
 
 
 -- BORDERS --
